f0dcf0da53198109b75c3a99a052ffdf542a5c25
wmatson <wmatson@live.com>
2020-06-25 09:04:36 -0600

    Birthday basic solution, with minimal tests

--- src/katas/birthday/core.clj
+++ src/katas/birthday/core.clj
@@ -2,36 +2,61 @@
   (:require
    [clojure.data.csv :as csv]
    [clojure.java.io :as io]
-   [clojure.string :as string]
    [postal.core :as postal])
   (:import
    (java.time LocalDate)
    (java.time.format DateTimeFormatter)
    (java.time.temporal ChronoUnit)))
 
+(def ^:private postal-host
+  {:host "localhost"
+   :user "azurediamond"
+   :pass "hunter2"
+   :port 2525})
+
+(defn- parse-date [date-str]
+  (LocalDate/parse date-str (DateTimeFormatter/ofPattern "yyyy/MM/dd")))
+
+(defn- load-employees []
+  (with-open [reader (io/reader (io/resource "birthday/employees.csv"))]
+    (let [[headers & data] (csv/read-csv reader)
+          kw-headers (map keyword headers)]
+      (->> (mapv zipmap (repeat kw-headers) data)
+           (map #(update % :date_of_birth parse-date))))))
+
+(defn- get-today []
+  (LocalDate/now))
+
+(defn- same-day? [& days]
+  (apply = (map #(vector (.getMonth %) (.getDayOfMonth %)) days)))
+
+(defn- format-email [{:keys [employee today]}]
+  {:from "me@example.com"
+   :to (:email employee)
+   :subject "Happy Birthday!"
+   :body (str "Happy Birthday " (:name employee) "! "
+              "Wow, you're "
+              (.between ChronoUnit/YEARS
+                        (:date_of_birth employee)
+                        today)
+              " already!")})
+
+(defn to-greetings [today employees]
+  (->> employees
+       (filter #(same-day? today (:date_of_birth %)))
+       (map #(format-email {:employee %
+                            :today today}))))
+
 (defn greet! []
-  (->> (io/resource "birthday/employees.csv")
-       (io/reader)
-       (csv/read-csv)
-       rest
-       (filter (fn [row]
-                 (string/ends-with?
-                   (row 2)
-                   (.format (LocalDate/now) (DateTimeFormatter/ofPattern "MM/dd")))))
-       (map (fn [row]
-                 (postal/send-message
-                  {:host "localhost"
-                   :user "azurediamond"
-                   :pass "hunter2"
-                   :port 2525}
-                  {:from "me@example.com"
-                   :to (row 1)
-                   :subject "Happy Birthday!"
-                   :body (str "Happy Birthday " (row 0) "! "
-                              "Wow, you're "
-                              (.between ChronoUnit/YEARS
-                                        (LocalDate/parse (row 2)
-                                                         (DateTimeFormatter/ofPattern "yyyy/MM/dd"))
-                                        (LocalDate/now))
-                              " already!")})))
-       doall))
+  (doseq [greeting (to-greetings (load-employees))]
+    (postal/send-message
+     postal-host
+     greeting)))
+
+(comment
+  (load-employees)
+
+  (.plusYears (get-today) 1)
+  (.getDayOfYear (.plusYears (get-today) 1))
+  (same-day? (get-today) (.plusYears (get-today) -4))
+  (greet!))
--- test/katas/test/birthday.clj
+++ test/katas/test/birthday.clj
@@ -1,7 +1,61 @@
 (ns katas.test.birthday
   (:require
-    [clojure.test :refer :all]
-    [katas.birthday.core :as birthday]))
+    [clojure.test :refer [deftest is are testing]]
+    [katas.birthday.core :as birthday])
+  (:import [java.time LocalDate]
+           [java.time.format DateTimeFormatter]))
+
+(deftest same-day-test
+  (testing "Non-leap-year-cases"
+    (are [expected days]
+         (= expected (apply #'birthday/same-day? days))
+      true [(LocalDate/of 2019 01 01) (LocalDate/of 2019 01 01)]
+      true [(LocalDate/of 2010 02 15) (LocalDate/of 2019 02 15)]
+      true [(LocalDate/of 2018 01 01) (LocalDate/of 2019 01 01)]
+      true [(LocalDate/of 2022 01 31) (LocalDate/of 2019 01 31)]
+      true [(LocalDate/of 2021 02 28) (LocalDate/of 2019 02 28)]
+      true [(LocalDate/of 2017 05 28) (LocalDate/of 2019 05 28)]
+      true [(LocalDate/of 2019 12 31) (LocalDate/of 2053 12 31)]
+      false [(LocalDate/of 2019 01 01) (LocalDate/of 2019 01 02)]))
+  (testing "Non-leap-year-cases"
+    (are [expected days]
+         (= expected (apply #'birthday/same-day? days))
+      true [(LocalDate/of 2020 01 01) (LocalDate/of 2020 01 01)]
+      true [(LocalDate/of 2020 02 15) (LocalDate/of 2020 02 15)]
+      true [(LocalDate/of 2018 01 01) (LocalDate/of 2020 01 01)]
+      true [(LocalDate/of 2020 01 31) (LocalDate/of 2020 01 31)]
+      true [(LocalDate/of 2019 02 28) (LocalDate/of 2020 02 28)]
+      ; true [(LocalDate/of 1996 02 29) (LocalDate/of 2019 02 28)])]
+      ; false [(LocalDate/of 1996 02 29) (LocalDate/of 2020 02 28)]
+      true [(LocalDate/of 2020 05 28) (LocalDate/of 2020 05 28)]
+      true [(LocalDate/of 2016 12 31) (LocalDate/of 2020 12 31)]
+      false [(LocalDate/of 2020 01 01) (LocalDate/of 2020 01 02)])))
+
+(defn- gen-employee [date-str id & [expected?]]
+  (cond->
+   {:name (str "Employee " id) 
+    :date_of_birth (LocalDate/parse date-str (DateTimeFormatter/ofPattern "yyyy-MM-dd"))
+    :email (str "e" id "@corp.com")}
+   expected? (assoc :expected true)))
+
+(deftest greeting-correct-individuals?
+  (let [today (LocalDate/of 2020 05 06)
+        emps [(gen-employee "1976-01-01" 1)
+              (gen-employee "1234-05-06" 2 true)
+              (gen-employee "1985-05-06" 3 true)
+              (gen-employee "1991-05-06" 4 true)
+              (gen-employee "1992-05-04" 5)
+     ;;Should future employees get birthday emails?
+              (gen-employee "3500-05-06" 6 true)]
+        expected-emails (->> emps
+                             (filter :expected)
+                             (map :email)
+                             set)
+        actual-emails (->> emps
+                           (birthday/to-greetings today)
+                           (map :to)
+                           set)]
+    (is (= expected-emails actual-emails))))
 
 (deftest greet!
   (testing "sends email to everyone with birthday today"
